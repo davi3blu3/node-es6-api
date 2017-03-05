8064a19001cf06d1896e8f2765116fad0ff01956
Dave Harned <dharned@gmail.com>
2017-03-05 09:29:54 -0600

    configure body parsers

--- config/express.js
+++ config/express.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import routes from '../server/routes';
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // mount all routes on /api path
 app.use('/api', routes);
