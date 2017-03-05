cfc70a500913c8a0329d3e4e95bd97b14224d7b7
Dave Harned <dharned@gmail.com>
2017-03-05 15:10:31 -0600

    define auth controller functions

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,9 @@
+// if this project were going to production it would be gitignored for security
+
 export default {
     env: 'development',
     db: 'mongodb://localhost:27017/node-es6-api-dev',
-    port: 3000
-}
+    port: 3000,
+    jwtSecret: 'my-api-secret',
+    jwtDuration: '2 hours'
+};
