bbb8f46d33763248aa97bc3d0df99dbb45f3b6fd
Dave Harned <dharned@gmail.com>
2017-03-05 00:29:00 -0600

    connect to database

---
+++ dist/config/env/development.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = {
+    env: 'development',
+    db: 'mongodb://localhost:27017/node-es6-api-dev',
+    port: 3000
+};
+module.exports = exports['default'];
