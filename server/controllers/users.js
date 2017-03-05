f95f8879d02e725d846670436a7b0afa8420566e
Dave Harned <dharned@gmail.com>
2017-03-05 10:54:49 -0600

    comment api controller functions

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,6 @@
 import User from '../models/user';
 
+// load the user from the database by id
 function load(req, res, next, id) {
     User.findById(id)
         .exec()
@@ -9,10 +10,12 @@ function load(req, res, next, id) {
         }, (e) => next(e));
 }
 
+// return a specific user's data
 function get(req, res) {
     return res.json(req.dbUser);
 }
 
+// create a new user in database from client data
 function create(req, res, next) {
     User.create({
         username: req.body.username,
@@ -23,6 +26,7 @@ function create(req, res, next) {
     }, (e) => next(e));
 }
 
+// combines new client data with specific user id to update db
 function update(req, res, next) {
     const user = req.dbUser;
     Object.assign(user, req.body);
@@ -32,6 +36,7 @@ function update(req, res, next) {
         (e) => next(e));
 }
 
+// queries db for all users, but limits results
 function list(req, res, next) {
     const { limit = 50, skip = 0 } = req.query;
     User.find()
@@ -42,6 +47,7 @@ function list(req, res, next) {
         (e) => next(e));
 }
 
+// delete a specific user from the database
 function remove(req, res, next) {
     const user = req.dbUser;
     user.remove()
